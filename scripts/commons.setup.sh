f7442b1de9a1f2325ee91ea4294ad434394af624
Victor Villas <mail@victor.villas>
2020-03-21 17:41:51 -0300

    Remove policy and role refs specified for cfn-init

--- scripts/commons.setup.sh
+++ scripts/commons.setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -xe
 FILES=$(dirname "$0")
-yum install -y jq python3 python3-devel python3-wheel
+yum install -y jq
+jsonvar() { jq -n --argjson doc "$1" -r "\$doc.$2"; }
 
 IMDSv1="http://169.254.169.254/latest"
 AWS_PARTITION=$(curl "$IMDSv1/meta-data/services/partition")
@@ -8,17 +9,18 @@ export AWS_PARTITION
 
 IAM_ROLE=$(curl "$IMDSv1/meta-data/iam/security-credentials")
 IAM_DOCUMENT=$(curl "$IMDSv1/meta-data/iam/security-credentials/$IAM_ROLE")
-AWS_ACCESS_KEY_ID=$(jq -n --argjson doc "$IAM_DOCUMENT" -r '$doc.AccessKeyId')
-AWS_SECRET_ACCESS_KEY=$(jq -n --argjson doc "$IAM_DOCUMENT" -r '$doc.SecretAccessKey')
-AWS_SECURITY_TOKEN=$(jq -n --argjson doc "$IAM_DOCUMENT" -r '$doc.Token')
+AWS_ACCESS_KEY_ID=$(jsonvar "$IAM_DOCUMENT" AccessKeyId)
+AWS_SECRET_ACCESS_KEY=$(jsonvar "$IAM_DOCUMENT" SecretAccessKey)
+AWS_SECURITY_TOKEN=$(jsonvar "$IAM_DOCUMENT" Token)
 export IAM_ROLE AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SECURITY_TOKEN
 
 EC2_DOCUMENT=$(curl "$IMDSv1/dynamic/instance-identity/document")
-AWS_REGION=$(jq -n --argjson doc "$EC2_DOCUMENT" -r '$doc.region')
-AWS_ACCOUNT_ID=$(jq -n --argjson doc "$EC2_DOCUMENT" -r '$doc.accountId')
-EC2_INSTANCE_ID=$(jq -n --argjson doc "$EC2_DOCUMENT" -r '$doc.instanceId')
+AWS_REGION=$(jsonvar "$EC2_DOCUMENT" region)
+AWS_ACCOUNT_ID=$(jsonvar "$EC2_DOCUMENT" accountId)
+EC2_INSTANCE_ID=$(jsonvar "$EC2_DOCUMENT" instanceId)
 export AWS_REGION AWS_ACCOUNT_ID EC2_INSTANCE_ID
 
+yum install -y python3 python3-pip python3-wheel python3-devel
 pip3 install awscurl
 EC2_HOST_IDENTIFIER="arn:$AWS_PARTITION:ec2:$AWS_REGION:$AWS_ACCOUNT_ID"
 EC2_HOST_IDENTIFIER="$EC2_HOST_IDENTIFIER:instance/$EC2_INSTANCE_ID"
@@ -45,18 +47,22 @@ while [ "$FERNET_KEY" = "" ]; do
 done
 export FERNET_KEY
 
-yum install -y gcc libcurl-devel openssl-devel
-PYCURL_SSL_LIBRARY=openssl pip3 install \
-    "celery[sqs]" "apache-airflow[celery,postgres,s3,crypto]==1.10.9"
+envreplace() { CONTENT=$(envsubst <"$1"); echo "$CONTENT" >"$1"; }
+envreplace /etc/sysconfig/airflow.env
+envreplace /etc/cfn/cfn-hup.conf
+envreplace /etc/cfn/hooks.d/cfn-auto-reloader.conf
+mapfile -t AIRFLOW_ENVS < /etc/sysconfig/airflow.env
+export "${AIRFLOW_ENVS[@]}"
 
-cp "$FILES"/systemd/*.path /lib/systemd/system/
-cp "$FILES"/systemd/*.service /lib/systemd/system/
+cp "$FILES"/systemd/*.{path,timer,service} /lib/systemd/system/
 cp "$FILES"/systemd/airflow.env /etc/sysconfig/airflow.env
 cp "$FILES"/systemd/airflow.conf /usr/lib/tmpfiles.d/airflow.conf
+find "$FILES" -type f -iname "*.sh" -exec chmod +x {} \;
+
 mkdir -p /etc/cfn/hooks.d
 cp "$FILES"/systemd/cfn-hup.conf /etc/cfn/cfn-hup.conf
 cp "$FILES"/systemd/cfn-auto-reloader.conf /etc/cfn/hooks.d/cfn-auto-reloader.conf
-find "$FILES" -type f -iname "*.sh" -exec chmod +x {} \;
+systemctl enable --now cfn-hup.service
 
 cd_agent() {
     yum install -y ruby
@@ -65,16 +71,8 @@ cd_agent() {
     ./install auto
 }
 
-mkdir /airflow && chown -R ec2-user: /airflow
+yum install -y gcc libcurl-devel openssl-devel
+export PYCURL_SSL_LIBRARY=openssl
+pip3 install "apache-airflow[celery,postgres,s3,crypto]==1.10.9" "celery[sqs]"
 mkdir /run/airflow && chown -R ec2-user: /run/airflow
-envreplace() {
-    CONTENT=$(envsubst <"$1")
-    echo "$CONTENT" >"$1"
-}
-envreplace /etc/sysconfig/airflow.env
-envreplace /etc/cfn/cfn-hup.conf
-envreplace /etc/cfn/hooks.d/cfn-auto-reloader.conf
-mapfile -t AIRFLOW_ENVS < /etc/sysconfig/airflow.env
-export "${AIRFLOW_ENVS[@]}"
-
-systemctl enable --now cfn-hup.service
+mkdir "$AIRFLOW_HOME" && chown -R ec2-user: "$AIRFLOW_HOME"
