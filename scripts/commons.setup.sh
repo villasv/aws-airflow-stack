580d77fa3a053be41a7716dc782f6e616c918c7c
Victor Villas <mail@victor.villas>
2020-04-25 22:20:10 -0300

    Upgrade airflow to 1.10.10
    As a bonus, it fixes #169

--- scripts/commons.setup.sh
+++ scripts/commons.setup.sh
@@ -86,7 +86,7 @@ export "${AIRFLOW_ENVS[@]}"
 
 yum install -y gcc libcurl-devel openssl-devel
 export PYCURL_SSL_LIBRARY=openssl
-pip3 install "apache-airflow[celery,postgres,s3,crypto]==1.10.9" "celery[sqs]"
+pip3 install "apache-airflow[celery,postgres,s3,crypto]==1.10.10" "celery[sqs]"
 mkdir "$AIRFLOW_HOME" && chown -R ec2-user: "$AIRFLOW_HOME"
 
 systemctl enable --now cfn-hup.service
